49ce0aa3d011dd936c41cdb23c16b6d219a52d96
guow <guow@kt.dtu.dk>
2021-08-14 23:36:17 +0200

    Added comments for DepartmentController

--- Three/Controllers/DepartmentController.cs
+++ Three/Controllers/DepartmentController.cs
@@ -16,25 +16,30 @@ namespace Three.Controllers
 
         public IOptions<ThreeOptions> ThreeOptions { get; }
 
+        // 在下面的构造器中进行了 Service 的注入
         public DepartmentController(IDepartmentService departmentService, IOptions<ThreeOptions> threeOptions)
         {
             _departmentService = departmentService;
             _threeOptions = threeOptions;
         }
 
+        // 建立一个默认的 Action
         public async Task<IActionResult> Index()
         {
             ViewBag.Title = "Department Index";
             var departments = await _departmentService.GetAll();
-            return View(departments);
+            return View(departments); // View 中可以传入参数
         }
 
         //[HTTPGet]
+        // 该 Action 负责跳转到添加页面，不是异步方法。 设一个空的model 返回回去 等用户添加数据
         public IActionResult Add()
         {
             ViewBag.Title = "Add Department";
             return View(new Department());
         }
+        
+        // 提交
         [HttpPost]
         public async Task<IActionResult> Add(Department model)
         {
